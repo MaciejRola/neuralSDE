37e27801e7ab3516985a12f104b9ab64d2a2cbe0
mr417808 <mr417808@students.mimuw.edu.pl>
2024-02-17 18:11:22 +0100

    changed starting price of instrument to 1, adjusted strikes and prices

--- neuralLSV.py
+++ neuralLSV.py
@@ -53,18 +53,16 @@ class NeuralLSV(nn.Module):
         if test_normal_variables is not None:
             self.test_normal_variables = test_normal_variables
 
-    def forward(self, options, test=False):  # torch version
-        maturities = (options['Expiration_date'].unique() * self.N_steps / self.Time_horizon).astype(int).tolist()
+    def forward(self, maturities, strikes, test=False):  # torch version
+        maturities = (maturities * self.N_steps / self.Time_horizon).astype(int).tolist()
         maturities_dict = dict(zip(maturities, range(self.n_maturities)))
-        strikes = torch.tensor(options['Strike'].unique(), device=self.device, dtype=torch.float).reshape(-1, 1)
+        strikes = torch.tensor(strikes, device=self.device, dtype=torch.float).reshape(-1, 1)
         S0 = self.S0
         V0 = self.V0
         rho = self.rho
         dt = self.dt
         n_maturities = len(maturities)
         n_strikes = len(strikes)
-
-        assert n_maturities * n_strikes == len(options)
 
         prices = torch.zeros(n_strikes, n_maturities, device=self.device, requires_grad=True)
         variance = torch.zeros(n_strikes, n_maturities, device=self.device, requires_grad=True)
@@ -131,17 +129,14 @@ class NeuralLSV(nn.Module):
         return prices, variance
 
 
-def train(model, options, batch_size, epochs, threshold=2e-5):
+def train(model, maturities, strikes, target, batch_size, epochs, threshold=2e-5):
     loss_fn = nn.MSELoss()
 
     model = model.to(model.device)
-
-    target_prices = torch.tensor(pd.pivot_table(options, values='Price', index='Strike', columns='Expiration_date').to_numpy(), requires_grad=True, device=model.device, dtype=torch.float)
-
     networks_SDE = [model.sigma_S, model.b_V, model.sigma_V]
     parameters_SDE = list(model.sigma_S.parameters()) + list(model.b_V.parameters()) + list(model.sigma_V.parameters()) + [model.rho, model.V0]
     optimizer_SDE = optim.Adam(parameters_SDE, lr=1e-3)
-    #scheduler_SDE = optim.lr_scheduler.MultiStepLR(optimizer_SDE, milestones=[500, 800], gamma=0.2, verbose=True)
+    scheduler_SDE = optim.lr_scheduler.MultiStepLR(optimizer_SDE, milestones=[500, 800], gamma=0.2, verbose=True)
     if model.use_hedging:
         optimizer_Hedging = optim.Adam(list(model.Hedging_Vanilla.parameters()), lr=1e-3)
 
@@ -169,12 +164,12 @@ def train(model, options, batch_size, epochs, threshold=2e-5):
                 optimizer_Hedging.zero_grad()
 
                 init_time = time.time()
-                prices, prices_var = model(options, test=False)
+                prices, prices_var = model(maturities, strikes, test=False)
                 time_forward = time.time() - init_time
                 itercount += 1
 
                 if optim_SDE:
-                    loss = loss_fn(prices, target_prices)
+                    loss = loss_fn(prices, target)
                     init_time = time.time()
                     loss.backward()
                     nn.utils.clip_grad_norm_(parameters_SDE, 5)
@@ -190,15 +185,15 @@ def train(model, options, batch_size, epochs, threshold=2e-5):
                     time_backward = time.time() - init_time
                     print(f'iteration {itercount}, loss={loss.item()}, time_forward={time_forward}, time_backward={time_backward}')
                     optimizer_Hedging.step()
-            #scheduler_SDE.step()
+            scheduler_SDE.step()
         else:
             for batch in range(0, 20 * batch_size, batch_size):
                 optimizer_SDE.zero_grad()
                 init_time = time.time()
-                prices, _ = model(options, test=False)
+                prices, _ = model(maturities, strikes, test=False)
                 time_forward = time.time() - init_time
                 itercount += 1
-                loss = loss_fn(prices, target_prices)
+                loss = loss_fn(prices, target)
                 init_time = time.time()
                 loss.backward()
                 nn.utils.clip_grad_norm_(parameters_SDE, 5)
@@ -207,15 +202,16 @@ def train(model, options, batch_size, epochs, threshold=2e-5):
                     print('loss is nan')
                 print(f'iteration {itercount}, loss={loss.item()}, time_forward={time_forward}, time_backward={time_backward}')
                 optimizer_SDE.step()
+            scheduler_SDE.step()
 
         with torch.no_grad():
-            prices_mean, prices_var = model(options, test=True)
-            print(f'Prices: \n{prices_mean}, \n Target: \n{target_prices}, \n Variance: \n{prices_var}')
+            prices_mean, prices_var = model(maturities, strikes, test=True)
+            print(f'Prices: \n{prices_mean}, \n Target: \n{target}, \n Variance: \n{prices_var}')
 
-        MSE = loss_fn(prices_mean, target_prices)
+        MSE = loss_fn(prices_mean, target)
         loss_val = torch.sqrt(MSE)
         print(f'epoch={epoch}, loss={loss_val.item()}')
-        with open("log_eval_LSV.txt", "w") as f:
+        with open("log_eval_LSV.txt", "a") as f:
             f.write(f'{epoch},{loss_val.item()}\n')
 
         LOSSES.append(loss_val.item())
@@ -241,7 +237,7 @@ def train(model, options, batch_size, epochs, threshold=2e-5):
             loss_val_best = loss_val
             print('loss_val_best', loss_val_best)
             filename = 'neuralLSV.pth.tar'
-            checkpoint = {'state_dict': model.state_dict(), 'pred': prices_mean, 'target_mat_T': target_prices}
+            checkpoint = {'state_dict': model.state_dict(), 'pred': prices_mean, 'target': target}
             torch.save(checkpoint, filename)
 
         if loss_val.item() < threshold:
@@ -250,57 +246,64 @@ def train(model, options, batch_size, epochs, threshold=2e-5):
     return model_best
 
 
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-    torch.cuda.empty_cache()
-else:
-    device = 'cpu'
-print(f'Using {device}')
+if __name__ == '__main__':
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        torch.cuda.empty_cache()
+    else:
+        device = 'cpu'
+    print(f'Using {device}')
 
-options = pd.read_csv('Data/Options_results.csv')
-options = options[options['Expiration_date'].isin([0.5, 1.0, 1.5, 2.0])]
+    options = pd.read_csv('Data/Options_results.csv')
+    options = options[options['Expiration_date'].isin([1.0, 2.0])]
+    maturities = options['Expiration_date'].unique()
+    strikes = options['Strike'].unique() / 100
+    target = torch.tensor(pd.pivot_table(options, values='Price', index='Strike', columns='Expiration_date').to_numpy(), requires_grad=True, dtype=torch.float) / 100
 
-# market parameter
-F0 = 100
-n_maturities = 4
-n_strikes = 21
-Time_horizon = 2
-rfr = 0.05
-S0 = F0 * np.exp(-rfr * Time_horizon)
+    # market parameter
+    F0 = 100 / 100
+    n_maturities = 2
+    n_strikes = 21
+    Time_horizon = 2
+    rfr = 0.05
+    S0 = F0 * np.exp(-rfr * Time_horizon)
 
-# model parameters
-num_layers = 4
-num_layers_hedging = 3
-layer_size = 50
-layer_size_hedging = 30
-dropout = 0.0
-use_hedging = True
-use_batchnorm = True
+    # model parameters
+    num_layers = 4
+    num_layers_hedging = 3
+    layer_size = 50
+    layer_size_hedging = 30
+    dropout = 0.0
+    use_hedging = True
+    use_batchnorm = True
 
-# simulation parameters
-batch_size = 40000
-epochs = 1000
-N_simulations = 20 * batch_size
-N_steps = n_maturities * 8
-period_length = N_steps // n_maturities
+    # simulation parameters
+    batch_size = 40000
+    epochs = 1000
+    N_simulations = 20 * batch_size
+    N_steps = 96
+    period_length = N_steps // n_maturities
 
-# Monte Carlo test data
-MC_samples_test = 200000
-test_normal_variables = torch.randn(2, MC_samples_test, N_steps, requires_grad=False)
-# We will use antithetic Brownian paths for testing
-test_normal_variables = torch.cat([test_normal_variables, -test_normal_variables], 0)
+    # Monte Carlo test data
+    MC_samples_test = 200000
+    test_normal_variables = torch.randn(2, MC_samples_test, N_steps, requires_grad=False)
+    # We will use antithetic Brownian paths for testing
+    test_normal_variables = torch.cat([test_normal_variables, -test_normal_variables], 0)
 
-LOSSES = []
-if use_hedging:
-    VARIANCES = []
+    LOSSES = []
+    if use_hedging:
+        VARIANCES = []
 
-model = NeuralLSV(device=device, batch_size=batch_size, dropout=dropout, use_batchnorm=use_batchnorm, use_hedging=use_hedging,
-                  N_simulations=N_simulations, N_steps=N_steps, Time_horizon=Time_horizon, period_length=period_length,
-                  S0=S0, n_maturities=n_maturities, n_strikes=n_strikes, rfr=rfr,
-                  num_layers=num_layers, layer_size=layer_size,
-                  num_layers_hedging=num_layers_hedging, layer_size_hedging=layer_size_hedging,
-                  test_normal_variables=test_normal_variables)
-summary(model)
-print('Model initiated')
-train(model, options=options, batch_size=batch_size, epochs=epochs, threshold=2e-5)
-print('Model trained')
+    with open("log_eval_LSV.txt", "w") as f:
+        f.write('Initialisation\n')
+
+    model = NeuralLSV(device=device, batch_size=batch_size, dropout=dropout, use_batchnorm=use_batchnorm, use_hedging=use_hedging,
+                      N_simulations=N_simulations, N_steps=N_steps, Time_horizon=Time_horizon, period_length=period_length,
+                      S0=S0, n_maturities=n_maturities, n_strikes=n_strikes, rfr=rfr,
+                      num_layers=num_layers, layer_size=layer_size,
+                      num_layers_hedging=num_layers_hedging, layer_size_hedging=layer_size_hedging,
+                      test_normal_variables=test_normal_variables)
+    summary(model)
+    print('Model initiated')
+    train(model, maturities=maturities, strikes=strikes, target=target, batch_size=batch_size, epochs=epochs, threshold=2e-5)
+    print('Model trained')
